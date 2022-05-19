c87cac70422307b35eb792d95f98fd26c44db069
Szym0nu7 <91138890+Szym0nu7@users.noreply.github.com>
2022-05-19 21:55:22 +0200

    Tiding up
    - Organized the scirpt
    - added right sidebar
    - added list
    - added rolling dice (somewhat)

--- 09-chinczyk-19.03.2022-20220328T153947Z-001/09-chinczyk-19.03.2022/script-arrays.js
+++ 09-chinczyk-19.03.2022-20220328T153947Z-001/09-chinczyk-19.03.2022/script-arrays.js
@@ -16,25 +16,17 @@ var field = [
 	[],
 	[]
 ];
-var n = 0;
-var ramN;
-var idram;
 
-var var1;
-var yd = 43;
-var gd = 47;
-var rd = 51;
-var bd = 55;
-
-var rampawn;
-var rampawns = [];
+//pawn variables
 
 var pawns =	["y58","y59","y60","y61","g62","g63","g64","g65","r66","r67","r68","r69","b70","b71","b72","b73"];
 
 var pcount = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-var rollnum;
-var totalroll = 0;
+var yd = 43;
+var gd = 47;
+var rd = 51;
+var bd = 55;
 
 field[0][58] = "y58";
 field[1][59] = "y59";
@@ -56,41 +48,141 @@ field[13][71] = "b71";
 field[14][72] = "b72";
 field[15][73] = "b73";
 
-var pawn;
-loading();
+var wins = [
+	{player:"yellow", score:0},
+	{player:"green", score:0},
+	{player:"red", score:0},
+	{player:"blue", score:0},
+]
+
+//other variables
+
+var moved = 1;
+var checkroll = 0;
 var checktrue = 0;
-var score = [0,0,0,0]
+var rollagain = 0;
+
+var diceoutput =""; 
+var plus;
+var rollnum;
+var n = 0;
+var var1;
+var pawn;
+var ramN;
+var idram;
+var rampawn;
+var rampawns = [];
+
+//test function
+
+function test(){
+
+}
+
+//main functions
+
+function rolling(){
+	if(moved == 1){
+	roll();
+	checkroll = 1;
+	if(rollnum == 6){plus=" + ";}else{plus="";}
+	diceoutput = diceoutput + rollnum.toString() + plus;
+	document.getElementById("diceout").innerHTML = diceoutput;
+	if(rollnum != 6){diceoutput="";}
+	moved = 0;
+	}
+}
+
+function checkfield (id){
+	idram = id;
+	checktrue = 0;
+	checkpawn();
+	if (checktrue == 1 && checkroll == 1){ 
+		checkroll = 0;
+		console.log(id + " = id")
+		field[ramN][id] = null;								
+									console.log(rampawn + " = rampawn")											
+		pawn = document.getElementById("a" + id);
+		pawn.style.backgroundImage = null;														
+		pcount[pawns.indexOf(rampawn)] = pcount[pawns.indexOf(rampawn)] + rollnum;												
+		id = id + rollnum; 											
+		rollnum = 0;													
+							console.log("pcount = " + pcount[pawns.indexOf(rampawn)])
+		if (pcount[pawns.indexOf(rampawn)] >= 39 ){
+				 if (rampawn.charAt(0) == "y") { id = yd; yd--; } // ID = 40 - 43
+			else if (rampawn.charAt(0) == "g") { id = gd; gd--; } // ID = 44 - 47
+			else if (rampawn.charAt(0) == "r") { id = rd; rd--; } // ID = 48 - 51
+			else if (rampawn.charAt(0) == "b") { id = bd; bd--; } // ID = 52 - 55
+		}
+		else if (id > 39){
+			id = id - 39;
+		}
+		field[ramN][id] = rampawn; 											
+		idram = id;
+		rampawns = [];
+		checkpawn2();
+		for (i = 0; i <= 15; i++){
+			if (rampawns[i]!= null && rampawn.charAt(0) != rampawns[i].charAt(0)){
+				field[i][id] = null;
+				var1 = rampawns[i].substr(1, 2);
+				field[i][var1] = rampawns[i];
+				pcount[pawns.indexOf(rampawns[i])] = 0;
+			}
+		}
+		loading();
+		if (rollagain == 1){
+			rollagain = 0;
+			alert("rollagain")	
+		}
+		moved = 1;
+		areyawinningson();
+	}
+}
+
+function fromBase(id){
+	idram = id;
+	checktrue = 0;
+	checkpawn();
+	if(checktrue == 1){
+		
+		pawn = document.getElementById("a" + id);
+		pawn.style.backgroundImage = null;
+		var start;
+		if(id >= 58 && id <= 61) start = 32;
+		else if(id >= 62 && id <= 65) start = 2;
+		else if(id >= 66 && id <= 69) start = 12;
+		else if(id >= 70 && id <= 73) start = 22;
+		field[ramN][id] = null;
+		field[ramN][start] = rampawn;
+		loading();
+	}
+}
+
+//checking functions
 
 function areyawinningson(){
 	if(yd == 39){
-		score[0]++;
+		wins[0].score++;
 		updategame();
 	}
 	else if(gd == 43){
-		score[1]++;
+		wins[1].score++;
 		updategame();
 	}
 	else if(rd == 47){
-		score[2]++;
+		wins[2].score++;
 		updategame();
 	}
 	else if(bd == 51){
-		score[3]++;
+		wins[3].score++;
 		updategame();
 	}
-}
-function updategame(){
-	
 }
 
 function checkpawn(){
 	n = 0;
-	field.forEach(truecheckpawn);
-		
-}
-function truecheckpawn(){
-	//console.log("n = " + n + "|| idram = " + idram)
-	
+	field.forEach(truecheckpawn);		
+}function truecheckpawn(){	
 	if (pawns.includes(field[n][idram]) == true){ 
 		console.log("n" + n)
 		console.log("pawn "+ pawns[n])
@@ -104,9 +196,7 @@ function truecheckpawn(){
 function checkpawn2(){
 	n = 0;
 	field.forEach(truecheckpawn2);
-}
-function truecheckpawn2(){
-	
+}function truecheckpawn2(){
 	if (pawns.includes(field[n][idram]) == true){ 
 		rampawns[n] = pawns[n];
 		ramN = n;
@@ -114,96 +204,7 @@ function truecheckpawn2(){
 	n++;	
 }
 
-function roll(){
-	
-	rollnum = Math.floor(Math.random() * 6) + 1;
-	console.log(rollnum + " = Roll-Number"); 			
-	totalroll = totalroll + rollnum;
-	if (rollnum == 6)
-	{
-		roll();
-	}
-	else{
-		if(totalroll != 0){								
-			console.log(totalroll + " = Total-Roll"); 	
-			console.log(""); 							
-		}	
-	}
-	
-}	
-
-function checkfield (id){
-	idram = id;
-	checktrue = 0;
-	checkpawn();
-							console.log(checktrue + " from checkfield / 0 or 1")
-	
-	if (checktrue == 1){ 
-		console.log(id + " = id")
-
-		field[ramN][id] = null;								
-							console.log(rampawn + " = rampawn")											
-		pawn = document.getElementById("a" + id);
-		pawn.style.backgroundImage = null;
-		roll(); 														
-		pcount[pawns.indexOf(rampawn)] = pcount[pawns.indexOf(rampawn)] + totalroll;												
-		id = id + totalroll; 											
-		totalroll = 0;													
-							console.log("pcount = " + pcount[pawns.indexOf(rampawn)])
-
-		if (pcount[pawns.indexOf(rampawn)] >= 39 ){
-				 if (rampawn.charAt(0) == "y") { id = yd; yd--; } // ID = 40 - 43
-			else if (rampawn.charAt(0) == "g") { id = gd; gd--; } // ID = 44 - 47
-			else if (rampawn.charAt(0) == "r") { id = rd; rd--; } // ID = 48 - 51
-			else if (rampawn.charAt(0) == "b") { id = bd; bd--; } // ID = 52 - 55
-		}
-		else if (id > 39){
-			id = id - 39;
-		}
-
-		field[ramN][id] = rampawn; 											
-		idram = id;
-		rampawns = [];
-		checkpawn2();
-		for (i = 0; i <= 15; i++){
-			if (rampawns[i]!= null && rampawn.charAt(0) != rampawns[i].charAt(0)){
-									console.log("is it here?")
-				field[i][id] = null;
-				var1 = rampawns[i].substr(1, 2);
-				field[i][var1] = rampawns[i];
-				pcount[pawns.indexOf(rampawns[i])] = 0;
-				
-			}
-		}
-		loading();
-		areyawinningson();
-	}
-	
-}
-
-function fromBase(id){
-	idram = id;
-	checktrue = 0;
-	checkpawn();
-									console.log(checktrue + " from base");
-	if(checktrue == 1){
-		
-		pawn = document.getElementById("a" + id);
-		pawn.style.backgroundImage = null;
-		//pawn = document.getElementById("a32");
-		//pawn.style.backgroundImage = "url('img/pawns/yellowpawn.png')";
-		var start;
-		if(id >= 58 && id <= 61) start = 32;
-		else if(id >= 62 && id <= 65) start = 2;
-		else if(id >= 66 && id <= 69) start = 12;
-		else if(id >= 70 && id <= 73) start = 22;
-									console.log("ramN = " + ramN);
-									console.log("rampawn = " + rampawn)
-		field[ramN][id] = null;
-		field[ramN][start] = rampawn;
-		loading();
-	}
-}
+//other functions
 
 function loading(){
     n = 0;
@@ -213,12 +214,29 @@ function loading(){
 	if (pawns[n].charAt(0) == "y")  tile.style.backgroundImage = "url('img/pawns/yellowpawn.png')";
 	else if (pawns[n].charAt(0) == "g")  tile.style.backgroundImage = "url('img/pawns/greenpawn.png')";
 	else if (pawns[n].charAt(0) == "r")  tile.style.backgroundImage = "url('img/pawns/redpawn.png')";
-	else if (pawns[n].charAt(0) == "b") tile.style.backgroundImage = "url('img/pawns/bluepawn.png')";
-   
+	else if (pawns[n].charAt(0) == "b") tile.style.backgroundImage = "url('img/pawns/bluepawn.png')"; 
     n++;
 }
 
-
-function test(){
-	
+function updategame(){
+	wins.sort(compare);
+	for(i=0; i<4; i++){
+		document.getElementById("p" + i).innerHTML = wins[i].player +": "+ wins[i].score + " pkt";
+	}
+}function compare( a, b ) {
+	if ( a.score > b.score ){
+	  return -1;
+	}
+	if ( a.score < b.score ){
+	  return 1;
+	}
+	return 0;
 }
+
+function roll(){
+	rollnum = Math.floor(Math.random() * 6) + 1;
+	console.log(rollnum + " = Roll-Number"); 			
+	if (rollnum == 6)	{
+		rollagain = 1;
+	}	
+}	
