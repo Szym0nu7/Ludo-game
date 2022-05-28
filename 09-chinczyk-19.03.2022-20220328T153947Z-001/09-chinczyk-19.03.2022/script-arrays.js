b7da85a28f59eeb08181bfebe13495532ed083df
Szym0nu7 <91138890+Szym0nu7@users.noreply.github.com>
2022-05-28 13:09:21 +0200

    EVERYTHING IS DOEN
    only a test run left

--- 09-chinczyk-19.03.2022-20220328T153947Z-001/09-chinczyk-19.03.2022/script-arrays.js
+++ 09-chinczyk-19.03.2022-20220328T153947Z-001/09-chinczyk-19.03.2022/script-arrays.js
@@ -19,9 +19,9 @@ var field = [
 
 //pawn variables
 
-var pawns =	["y58","y59","y60","y61","g62","g63","g64","g65","r66","r67","r68","r69","b70","b71","b72","b73"];
+var pawns = ["y58", "y59", "y60", "y61", "g62", "g63", "g64", "g65", "r66", "r67", "r68", "r69", "b70", "b71", "b72", "b73"];
 
-var pcount = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var pcount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 var yd = 43;
 var gd = 47;
@@ -49,13 +49,14 @@ field[14][72] = "b72";
 field[15][73] = "b73";
 
 var wins = [
-	{player:"yellow", score:0},
-	{player:"green", score:0},
-	{player:"red", score:0},
-	{player:"blue", score:0},
+	{ player: "yellow", score: 0 },
+	{ player: "green", score: 0 },
+	{ player: "red", score: 0 },
+	{ player: "blue", score: 0 },
 ]
 
-var curplr = 1;
+var culprit = 1;
+var curplr = "Yellow"
 
 //other variables
 
@@ -66,7 +67,7 @@ var rollagain = 0;
 var playercheck = 0;
 var isnt = 0;
 
-var diceoutput =""; 
+var diceoutput = "";
 var plus;
 var rollnum;
 var n = 0;
@@ -77,239 +78,280 @@ var idram;
 var rampawn;
 var rampawns = [];
 var convertedcurplr;
+var place = 1;
+var object;
+var movedtiles
 
 //test function
 
-function test(){
-	
+function test() {
+
 }
 
 //main functions
 
-function turns(){
+function turns() {
 	checkroll = 0;
+	//display current player
+	//tell player to roll document.getElementById("tip").innerHTML = "Rzuć kością";
 	//player uses rolling
+	//tell player to chose pawn document.getElementById("tip").innerHTML = "Wybierz pionka";
 	//player choses pawn
+	//eventually tell player to roll again  document.getElementById("tip").innerHTML = "Rzuć kością";
 	//eventually rolls again
-	curplr++;
-	if (curplr == 5) curplr = 1;
+	culprit++;
+	if (culprit == 5) culprit = 1;
 	//repeat
 }
 
-function rolling(){
-	if(moved == 1){
-		document.getElementById("playerwhich").innerHTML = curplr;
+function rolling() {
+	document.getElementById("playerwhich").innerHTML = curplr;
+	if (moved == 1) {
 		roll();
 		checkroll = 1;
-		document.getElementById("die").src = "img/dice/dice"+rollnum+".png";
+		document.getElementById("die").src = "img/dice/dice" + rollnum + ".png";
 		moved = 0;
-		console.log("culprit " + curplr)
+		console.log("culprit " + culprit)
 		isntpawn();
-		if (isnt == 4){
+		if (isnt == 4) {
 			console.log("inst")
 			isnt = 0;
-			
+
 			if (rollagain == 0) {
 				checkroll = 0;
 				playerprogress();
-			}
-		}
+			} else document.getElementById("tip").innerHTML = "Choose pawn";
+		} else document.getElementById("tip").innerHTML = "Choose pawn";
 	}
-}function roll(){
+} function roll() {
 	rollnum = Math.floor(Math.random() * 6) + 1;
-	console.log(rollnum + " = Roll-Number"); 			
-	if (rollnum == 6)	{
+	console.log(rollnum + " = Roll-Number");
+	if (rollnum == 6) {
 		rollagain = 1;
-	}	
-}	
+	}
+}
 
-function checkfield (id){
+function checkfield(id) {
 	idram = id;
 	chec();
-	if (checktrue == 1 && checkroll == 1 && playercheck == 1){ 
+	if (checktrue == 1 && checkroll == 1 && playercheck == 1) {
 		checclear();
 		console.log(id + " = id")
-		field[ramN][id] = null;								
-									console.log(rampawn + " = rampawn")											
+		field[ramN][id] = null;
+		console.log(rampawn + " = rampawn")
 		pawn = document.getElementById("a" + id);
-		pawn.style.backgroundImage = null;														
-		pcount[pawns.indexOf(rampawn)] = pcount[pawns.indexOf(rampawn)] + rollnum;												
-		id = id + rollnum; 											
-		rollnum = 0;													
-							console.log("pcount = " + pcount[pawns.indexOf(rampawn)])
-		if (pcount[pawns.indexOf(rampawn)] >= 39 ){
-				 if (rampawn.charAt(0) == "y") { id = yd; yd--; } // ID = 40 - 43
+		pawn.style.backgroundImage = null;
+		pcount[pawns.indexOf(rampawn)] = pcount[pawns.indexOf(rampawn)] + rollnum;
+		//sanimateobjcet();
+		id = id + rollnum;
+		rollnum = 0;
+		console.log("pcount = " + pcount[pawns.indexOf(rampawn)])
+		if (pcount[pawns.indexOf(rampawn)] >= 39) {
+			if (rampawn.charAt(0) == "y") { id = yd; yd--; } // ID = 40 - 43
 			else if (rampawn.charAt(0) == "g") { id = gd; gd--; } // ID = 44 - 47
 			else if (rampawn.charAt(0) == "r") { id = rd; rd--; } // ID = 48 - 51
 			else if (rampawn.charAt(0) == "b") { id = bd; bd--; } // ID = 52 - 55
 		}
-		else if (id > 39){
+		else if (id > 39) {
 			id = id - 39;
 		}
-		field[ramN][id] = rampawn; 											
+		field[ramN][id] = rampawn;
 		idram = id;
 		rampawns = [];
 		checkpawn2();
-		for (i = 0; i <= 15; i++){
-			if (rampawns[i]!= null && rampawn.charAt(0) != rampawns[i].charAt(0)){
+		for (i = 0; i <= 15; i++) {
+			if (rampawns[i] != null && rampawn.charAt(0) != rampawns[i].charAt(0)) {
 				field[i][id] = null;
 				var1 = rampawns[i].substr(1, 2);
 				field[i][var1] = rampawns[i];
 				pcount[pawns.indexOf(rampawns[i])] = 0;
 			}
 		}
-		if (rollagain == 1){
+		if (rollagain == 1) {
 			rollagain = 0;
-			alert("rollagain")	
-		}else playerprogress(); 
+		} else playerprogress();
 		loading();
 		moved = 1;
 		areyawinningson();
+		document.getElementById("tip").innerHTML = "Roll";
 	}
 }
 
-function fromBase(id){
+function fromBase(id) {
 	idram = id;
 	chec();
-	if(checktrue == 1 && rollagain == 1 && checkroll == 1 && playercheck == 1){
+	if (checktrue == 1 && rollagain == 1 && checkroll == 1 && playercheck == 1) {
 		checclear();
 
 		pawn = document.getElementById("a" + id);
 		pawn.style.backgroundImage = null;
 		var start;
-		if(id >= 58 && id <= 61) start = 32;
-		else if(id >= 62 && id <= 65) start = 2;
-		else if(id >= 66 && id <= 69) start = 12;
-		else if(id >= 70 && id <= 73) start = 22;
+		if (id >= 58 && id <= 61) start = 32;
+		else if (id >= 62 && id <= 65) start = 2;
+		else if (id >= 66 && id <= 69) start = 12;
+		else if (id >= 70 && id <= 73) start = 22;
 		field[ramN][id] = null;
 		field[ramN][start] = rampawn;
-		if (rollagain == 1){
+		if (rollagain == 1) {
 			rollagain = 0;
-			alert("rollagain")	
-		}else playerprogress();
+		} else playerprogress();
 		moved = 1;
 		loading();
+		document.getElementById("tip").innerHTML = "Roll";
 	}
 }
 
 //checking functions
 
-function checkplayer(){
-	
-	if (rampawn.charAt(0) == "y")convertedcurplr = 1;
-	else if (rampawn.charAt(0) == "g")convertedcurplr = 2;
-	else if (rampawn.charAt(0) == "r")convertedcurplr = 3;
-	else if (rampawn.charAt(0) == "b")convertedcurplr = 4;
-	if (convertedcurplr == curplr) playercheck = 1;
+function checkplayer() {
+
+	if (rampawn.charAt(0) == "y") convertedcurplr = 1;
+	else if (rampawn.charAt(0) == "g") convertedcurplr = 2;
+	else if (rampawn.charAt(0) == "r") convertedcurplr = 3;
+	else if (rampawn.charAt(0) == "b") convertedcurplr = 4;
+	if (convertedcurplr == culprit) playercheck = 1;
 }
 
-function areyawinningson(){
-	if(yd == 39){
-		wins[0].score++;
+function areyawinningson() {
+	if (yd == 39) {
+		wins[0].score = place;
+		place++;
 		updategame();
 	}
-	else if(gd == 43){
-		wins[1].score++;
+	else if (gd == 43) {
+		wins[1].score = place;
+		place++;
 		updategame();
 	}
-	else if(rd == 47){
-		wins[2].score++;
+	else if (rd == 47) {
+		wins[2].score = place;
+		place++;
 		updategame();
 	}
-	else if(bd == 51){
-		wins[3].score++;
+	else if (bd == 51) {
+		wins[3].score = place;
+		place++;
 		updategame();
 	}
 }
 
-function checkpawn(){
+function checkpawn() {
 	n = 0;
-	field.forEach(truecheckpawn);		
-}function truecheckpawn(){	
-	if (pawns.includes(field[n][idram]) == true){ 
+	field.forEach(truecheckpawn);
+} function truecheckpawn() {
+	if (pawns.includes(field[n][idram]) == true) {
 		console.log("n" + n)
-		console.log("pawn "+ pawns[n])
+		console.log("pawn " + pawns[n])
 		rampawn = pawns[n];
 		ramN = n;
 		checktrue = 1;
 	}
-	n++;	
+	n++;
 }
 
-function checkpawn2(){
+function checkpawn2() {
 	n = 0;
 	field.forEach(truecheckpawn2);
-}function truecheckpawn2(){
-	if (pawns.includes(field[n][idram]) == true){ 
+} function truecheckpawn2() {
+	if (pawns.includes(field[n][idram]) == true) {
 		rampawns[n] = pawns[n];
 		ramN = n;
 	}
-	n++;	
+	n++;
 }
 
-function isntpawn(){
+function isntpawn() {
 	n = 1;
 	wins.forEach(trueisntpawn);
-}function trueisntpawn(){
-	var shittydesign = curplr * 4 - n;
-	console.log("shittydesign "+shittydesign + " " +pawns[shittydesign])	
-	if (field[shittydesign].indexOf(pawns[shittydesign]) > 40){ 
+} function trueisntpawn() {
+	var shittydesign = culprit * 4 - n;
+	console.log("shittydesign " + shittydesign + " " + pawns[shittydesign])
+	if (field[shittydesign].indexOf(pawns[shittydesign]) > 40) {
 		isnt++;
 	}
 	n++;
 }
 //animations
 
+function animateobjcet() {
 
+	if (rampawn.charAt(0) == "y") object = "url('img/pawns/yellowpawn.png')";
+	else if (rampawn.charAt(0) == "g") object = "url('img/pawns/greenpawn.png')";
+	else if (rampawn.charAt(0) == "r") object = "url('img/pawns/redpawn.png')";
+	else if (rampawn.charAt(0) == "b") object = "url('img/pawns/bluepawn.png')";
+	movedtiles = 1;
+	var bullshit = setInterval(animate(), 300);
+
+} function animate(t) {
+	if (movedtiles <= rollnum) {
+		var num = idram + movedtiles;
+		var tile = document.getElementById("a" + num);
+		tile.style.backgroundImage = object;
+		movedtiles++;
+		console.log("bitch")
+
+	}//else clearInterval(bullshitt);
+}
 
 //other functions
 
-function chec(){
+function chec() {
 	checktrue = 0;
 	checkpawn();
 	checkplayer();
-}function checclear(){
+} function checclear() {
 	checkroll = 0;
 	playercheck = 0;
 }
 
-function loading(){
-    n = 0;
-    field.forEach(finding);
-}function finding(){
-    var tile = document.getElementById("a" + field[n].indexOf(pawns[n]));
-	if (pawns[n].charAt(0) == "y")  tile.style.backgroundImage = "url('img/pawns/yellowpawn.png')";
-	else if (pawns[n].charAt(0) == "g")  tile.style.backgroundImage = "url('img/pawns/greenpawn.png')";
-	else if (pawns[n].charAt(0) == "r")  tile.style.backgroundImage = "url('img/pawns/redpawn.png')";
-	else if (pawns[n].charAt(0) == "b") tile.style.backgroundImage = "url('img/pawns/bluepawn.png')"; 
-    n++;
+function loading() {
+	document.getElementById("playerwhich").innerHTML = curplr;
+	n = 0;
+	field.forEach(finding);
+} function finding() {
+	var tile = document.getElementById("a" + field[n].indexOf(pawns[n]));
+	if (pawns[n].charAt(0) == "y") tile.style.backgroundImage = "url('img/pawns/yellowpawn.png')";
+	else if (pawns[n].charAt(0) == "g") tile.style.backgroundImage = "url('img/pawns/greenpawn.png')";
+	else if (pawns[n].charAt(0) == "r") tile.style.backgroundImage = "url('img/pawns/redpawn.png')";
+	else if (pawns[n].charAt(0) == "b") tile.style.backgroundImage = "url('img/pawns/bluepawn.png')";
+	n++;
 }
 
-function updategame(){
+function updategame() {
 	wins.sort(compare);
-	for(i=0; i<4; i++){
-		document.getElementById("p" + i).innerHTML = wins[i].player +": "+ wins[i].score + " pkt";
+	for (i = 0; i < 4; i++) {
+		if (wins[i].score == 0) document.getElementById("p" + i).innerHTML = "-";
+		else document.getElementById("p" + i).innerHTML = wins[i].player;
 	}
-}function compare( a, b ) {
-	if ( a.score > b.score ){
-	  return -1;
+} function compare(a, b) {
+	if (a.score < b.score) {
+		return -1;
 	}
-	if ( a.score < b.score ){
-	  return 1;
+	if (a.score > b.score) {
+		return 1;
 	}
 	return 0;
 }
 
-function playerprogress(){
-	curplr++;
+function playerprogress() {
+	document.getElementById("playerwhich").innerHTML = curplr;
+	culprit++;
 	isnt = 0;
-	if (curplr == 5) curplr = 1;
+	if (culprit == 5) culprit = 1;
+	switch (culprit) {
+		case 1: curplr = "Yellow"; break;
+		case 2: curplr = "Green"; break;
+		case 3: curplr = "Red"; break;
+		case 4: curplr = "Blue"; break;
+	}
+	console.log("curplr " + curplr + " " + culprit)
+	document.getElementById("playerwhich").innerHTML = curplr;
 	moved = 1;
 }
 
-function toggleMenu(){
-    let navigation = document.querySelector('.navigation');
-    let toggle = document.querySelector('.toggle');
-    navigation.classList.toggle('active');
-    toggle.classList.toggle('active');
+function toggleMenu() {
+	let navigation = document.querySelector('.navigation');
+	let toggle = document.querySelector('.toggle');
+	navigation.classList.toggle('active');
+	toggle.classList.toggle('active');
 }
